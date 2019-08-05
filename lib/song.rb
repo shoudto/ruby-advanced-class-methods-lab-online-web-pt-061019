5f58a7766e04eada92842c5603b4e541d1a662b3
Oscar Shoudt <shoudto@gmail.com>
2019-08-05 19:03:23 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -36,7 +36,11 @@ class Song
   end
 
   def self.find_or_create_by_name(name)
-
+    if !self.find_by_name(name)
+      self.find_by_name(name)
+    else 
+      self.find_by_name(name)
+    end 
   end
 
 end
